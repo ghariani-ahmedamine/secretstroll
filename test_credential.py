250ebbc1a5f5500c735367e208e4bf92d48e4e41
Fab1205 <42958813+Fab1205@users.noreply.github.com>
2023-05-12 13:49:59 +0200

    Create test_credential.py
    Add the example test file for credential

---
+++ test_credential.py
@@ -0,0 +1,14 @@
+
+# Example tests
+def test_1():
+    assert "123" == "123"
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+def test_some_primes():
+    assert 37 in {
+        num
+        for num in range(2, 50)
+        if not any(num % div == 0 for div in range(2, num))
+    }
